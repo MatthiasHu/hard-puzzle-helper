86445d6d6a8e377916fc20740eb34969c3fe305e
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2018-01-06 14:30:06 +0100

    EdgesFromImage module

---
+++ EdgesFromImage.hs
@@ -0,0 +1,43 @@
+module EdgesFromImage
+  ( edgesFromImage
+  , edgesAndCornersFromImage
+  , edge64ToImage
+  ) where
+
+import FindCorners
+import ComponentSet
+import Edge
+import SquareGrid
+
+import Codec.Picture
+
+
+edgesAndCornersFromImage :: Image PixelRGB8 -> ([Edge64], [(Int, Int)])
+edgesAndCornersFromImage img =
+  ( map (uncurry $ edgeFromSet component)
+      [ (lt, rt)
+      , (lb, lt)
+      , (rb, lb)
+      , (rt, rb)
+      ]
+  , corners
+  )
+  where
+    w = imageWidth img
+    h = imageHeight img
+    component = componentSet img isDark (w `div` 2, h `div` 2)
+    corners = findCorners $ boundary component
+    [lb, rb, lt, rt] = corners
+
+isDark :: PixelRGB8 -> Bool
+isDark (PixelRGB8 r g b) = (r `div` 3 + g `div` 3 + b `div` 3) < 64
+
+edgesFromImage :: Image PixelRGB8 -> [Edge64]
+edgesFromImage = fst . edgesAndCornersFromImage
+
+edge64ToImage :: Edge64 -> Image PixelRGB8
+edge64ToImage e = generateImage generator 64 64
+  where
+    generator x y = if edge64Pixel e (x, (63-y)) then up else down
+    up = PixelRGB8 255 255 255
+    down = PixelRGB8 0 0 0
--- Main.hs
+++ Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import Edge
-import FindCorners
-import ComponentSet
+import EdgesFromImage
 import SquareGrid
 
 -- (using JuicyPixels)
@@ -22,39 +20,9 @@ main = do
     saveBmpImage ("edge-"++show i++".bmp")
       (ImageRGB8 $ edge64ToImage edge)
 
-edgesAndCornersFromImage :: Image PixelRGB8 -> ([Edge64], [(Int, Int)])
-edgesAndCornersFromImage img =
-  ( map (uncurry $ edgeFromSet component)
-      [ (lt, rt)
-      , (lb, lt)
-      , (rb, lb)
-      , (rt, rb)
-      ]
-  , corners
-  )
-  where
-    w = imageWidth img
-    h = imageHeight img
-    component = componentSet img isDark (w `div` 2, h `div` 2)
-    corners = findCorners $ boundary component
-    [lb, rb, lt, rt] = corners
-
-isDark :: PixelRGB8 -> Bool
-isDark (PixelRGB8 r g b) = (r `div` 3 + g `div` 3 + b `div` 3) < 64
-
-edgesFromImage :: Image PixelRGB8 -> [Edge64]
-edgesFromImage = fst . edgesAndCornersFromImage
-
 replacePixels :: (Pixel px) =>
   px -> Image px -> Set.Set (Int, Int) -> Image px
 replacePixels p img s =
   generateImage generator (imageWidth img) (imageHeight img)
   where
     generator x y = if (x, -y) `Set.member` s then p else pixelAt img x y
-
-edge64ToImage :: Edge64 -> Image PixelRGB8
-edge64ToImage e = generateImage generator 64 64
-  where
-    generator x y = if edge64Pixel e (x, (63-y)) then up else down
-    up = PixelRGB8 255 255 255
-    down = PixelRGB8 0 0 0
