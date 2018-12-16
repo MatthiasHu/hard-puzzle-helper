2d88691d4c417b079007300ca332661423ef6e69
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2018-12-16 17:28:54 +0100

    allImagesToEdges

--- Main.hs
+++ Main.hs
@@ -5,19 +5,53 @@ import SquareGrid
 
 -- (using JuicyPixels)
 import Codec.Picture
+import System.FilePath
+import System.Directory
 import qualified Data.Set as Set
 import Control.Monad
 
 
 main :: IO ()
-main = do
-  Right img <- fmap (convertRGB8) <$> readImage "test.jpg"
+main = allImagesToEdges
+
+allImagesToEdges :: IO ()
+allImagesToEdges = do
+  let
+    imagesPath = "input/images"
+    edgesPath = "input/pieces"
+    extension = ".jpg"
+  imagePaths <- filter ((==extension) . takeExtension) <$>
+    listDirectory imagesPath
+  putStrLn $ unwords
+    [ "found", show (length imagePaths)
+    , extension, "files in", imagesPath ]
+  putStrLn $ unwords ["saving edges to", edgesPath, "..."]
+  forM_ imagePaths $ \path -> do
+    putStrLn path
+    imageToEdges (imagesPath </> path) edgesPath
+
+imageToEdges :: FilePath -> FilePath -> IO ()
+imageToEdges imagePath edgesPath = do
+  readImage imagePath >>= \imgEither -> case imgEither of
+    Left err -> putStrLn $ unwords ["can't load image:", err]
+    Right img -> do
+      let
+        es = edgesFromImage (convertRGB8 img)
+        compass = ["north", "west", "south", "east"]
+        name dir = takeBaseName imagePath ++ "-" ++ dir
+      forM_ (zip es compass) $ \(e, dir) ->
+        saveBmpImage (edgesPath </> name dir <.> "bmp")
+          (ImageRGB8 $ edge64ToImage e)
+
+debugSingleImage :: FilePath -> IO ()
+debugSingleImage path = do
+  Right img <- fmap (convertRGB8) <$> readImage path
   let (edges, corners) = edgesAndCornersFromImage img
       cornersImg = replacePixels (PixelRGB8 255 0 0) img
         (Set.fromList $ corners ++ concatMap neighbours [head corners])
-  saveBmpImage "corners.bmp" $ ImageRGB8 cornersImg
+  saveBmpImage "debug-out/corners.bmp" $ ImageRGB8 cornersImg
   forM_ (zip edges [0..]) $ \(edge, i) ->
-    saveBmpImage ("edge-"++show i++".bmp")
+    saveBmpImage ("debug-out/edge-"++show i++".bmp")
       (ImageRGB8 $ edge64ToImage edge)
 
 replacePixels :: (Pixel px) =>
